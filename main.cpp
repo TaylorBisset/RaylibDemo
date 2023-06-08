400756fd19c9029834cf3f6dba256c4d7093c127
Taylor Bisset <taylorbisset@gmail.com>
2023-06-08 10:28:41 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "raylib.h"
+
+
+int main(){
+	int width = 500;
+	int height = 500;
+
+	InitWindow(width, height, "window title");
+	SetTargetFPS(60);
+
+
+	while (!WindowShouldClose()){
+		BeginDrawing();
+
+		ClearBackground(WHITE);
+
+
+
+		EndDrawing();
+	}
+
+
+	CloseWindow();
+
+	return 0;
+}
