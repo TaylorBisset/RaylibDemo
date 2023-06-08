eea8d83ea19cda4b4eb8f602d0b362eab9c055a8
Taylor Bisset <taylorbisset@gmail.com>
2023-06-08 11:47:27 -0700

    Adjust variables.

--- main.cpp
+++ main.cpp
@@ -5,20 +5,19 @@ int main(){
 	int height = 500;
 
 	int startX = 0;
-	int endX = 250;
 	int startY = 0;
+	
+	int endX = 250;
 	int endY = 250;
 
-	InitWindow(width, height, "window title");
+	InitWindow(width, height, "Circular Drawing");
 	SetTargetFPS(60);
 
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
-
 		ClearBackground(WHITE);
-
 		DrawLine(0, 250, 500, 250, BLACK); // draws thin black horizontal line in the middle of the window.
 
 
